5058908fcd2c34ef4b102bfa22ce5ea59c44eaee
ohad shalom <ohad5855@gmail.com>
2022-04-14 10:56:22 +0300

    Documentation added

--- src/routes/auth_routes.ts
+++ src/routes/auth_routes.ts
@@ -5,9 +5,66 @@ import authenticate from "../middleware/auth_middleware"
 const router = express.Router()
 
 
+/**
+* @swagger
+* tags:
+*   name: Auth
+*   description: The Authentication API
+*/
+
+/**
+* @swagger
+*   components:
+*       securitySchemes:
+*           bearerAuth:
+*               type: http
+*               scheme: bearer
+*               bearerFormat: JWT
+*/
+
+/**
+* @swagger
+*   components:
+*       schemas:
+*           User:
+*               type: object
+*           required:
+*               - email
+*               - password
+*           properties:
+*               email:
+*                   type: string
+*                   description: The user email
+*               password:
+*               type: string
+*               description: The user password
+*           example:
+*               email: 'bob@gmail.com'
+*               password: '123456'
+*/
+
 router.post('/login',Auth.login)
 
-router.post('/register',Auth.register)
+/**
+* @swagger
+*    /auth/register:
+*       post:
+*           summary: registers a new user
+*           tags: [Auth]
+*       requestBody:
+*           required: true
+*       content:
+*           application/json:
+*       schema:
+*           $ref: '#/components/schemas/User'
+*       responses:
+*           200:
+*               description: Register success return access and refresh token
+*               content:
+*                   application/json:
+*                       schema:
+*                           $ref: '#/components/schemas/User'
+*/
 router.post('/register',Auth.register)
 
 router.post('/refresh',Auth.renewToken)
--- src/server.ts
+++ src/server.ts
@@ -26,4 +26,27 @@ app.use("/post",post_route)
 import auth_route from './routes/auth_routes'
 app.use("/auth",auth_route)
 
+import swaggerUI from "swagger-ui-express"
+import swaggerJsDoc from "swagger-jsdoc"
+
+
+if (process.env.NODE_ENV == "development") {
+
+    const options = {
+        definition: {
+            openapi: "3.0.0",
+            info: {
+                title: "Library API",
+                version: "1.0.0",
+                description: "A simple Express Library API",
+            },
+            servers: [{url: "http://localhost:3000",},],
+        },
+        apis: ["./src/routes/*.ts"],
+    };
+    const specs = swaggerJsDoc(options);
+    app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+    }
+
+
 export = app
