b5c438c9ea3f718d1f6129d657845dbd4ded48de
Ohad Shalom <ohadshalom@Ohads-MacBook-Pro.local>
2022-04-17 19:31:26 +0300

    socket tests refactoring

--- src/socket_server.ts
+++ src/socket_server.ts
@@ -2,10 +2,17 @@ import commonHandler from "./events/common"
 import { Server } from "socket.io"
 import http from 'http';
 import jwt from "jsonwebtoken"
+import {createClient} from "redis"
+import {createAdapter} from "@socket.io/redis-adapter"
 
 const socketServer = (server: http.Server): Server => {
     const io = new Server(server);
 
+    const pubClient = createClient({url:process.env.REDDIS_URL})
+    const subClient = pubClient.duplicate()
+
+    io.adapter(createAdapter(pubClient,subClient))
+    
     io.use(async (socket, next) => {
         let token = socket.handshake.auth.token;
         if(token == null) return next(new Error('Authentication error'))
--- src/test/socket.test.ts
+++ src/test/socket.test.ts
@@ -5,53 +5,84 @@ import User from '../models/user_model'
 import request from 'supertest'
 
 
-let clientSocket:Socket
-const email = 'test@socket.com'
-const pass = '1234567890'
 
-let accessToken = ""
+type UserInfo={
+    email:string,
+    password:string,
+    accessToken:string,
+    clientSocket:Socket
+}
+
+let user1:UserInfo ={
+    email:"user1@socket.com",
+    password:"1234567890",
+    accessToken:" ",
+    clientSocket:null
+} 
+let user2:UserInfo ={
+    email:"user2@socket.com",
+    password:"1234567890",
+    accessToken:" ",
+    clientSocket:null
+} 
 
 beforeAll(async () => {
-    console.log("BEFOREALLLLLLL")
-    await User.remove({"email":email})    
+    await User.remove({"email":user1.email})    
+    await User.remove({"email":user2.email})    
+
 });
 
 afterAll(async () => {
-    clientSocket.close()
-    server.close()
-    await User.remove({"email":email})
+    user1.clientSocket.close()
+    user2.clientSocket.close()
+    await User.remove({"email":user1.email})    
+    await User.remove({"email":user2.email})  
     mongoose.connection.close();
+    server.close()
 });
 
 describe("Socket IO server test", () => {
+    const registerUser=async (userInfo:UserInfo)=>{
 
-    test("register user for access",async ()=>{
         const response = await request(server)
             .post('/auth/register')
-            .send({"email":email , 'password':pass})
+            .send({"email":userInfo.email , 'password':userInfo.password})
         expect(response.statusCode).toEqual(200)
-        accessToken = response.body.accessToken
+        userInfo.accessToken = response.body.accessToken
 
+    }
+
+    test("register user for access",async ()=>{
+        await registerUser(user1)
+        await registerUser(user2)
     })
 
-    test("open client connection",(done)=>{
-        clientSocket = Client("http://localhost:" + process.env.PORT,{
+    const openClientConnection = (userInfo:UserInfo,done)=>{
+
+        userInfo.clientSocket = Client("http://localhost:" + process.env.PORT,{
             auth: {
-                token: 'barrer ' + accessToken
+                token: 'barrer ' + userInfo.accessToken
             } 
         })
-        clientSocket.on("connect", async()=>{
+        userInfo.clientSocket.on("connect", ()=>{
             done();
         });
+    }
+    test("open client 1 connection",(done)=>{
+        openClientConnection(user1,done)
+    })
+    test("open client 2 connection",(done)=>{
+        openClientConnection(user2,done)
     })
 
+    
     test("test echo event", (done) => {
-        clientSocket.on("common:echo", (arg: any) => {
+        user1.clientSocket.on("common:echo", (arg: any) => {
             expect(arg).toBe("echo message");
             done();
         });
-        clientSocket.emit("common:echo", "echo message")
+        user1.clientSocket.emit("common:echo", "echo message")
     });
-    
+
 });
 
